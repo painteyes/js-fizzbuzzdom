64ee4da416abe9e8e3f60e798cf0874f78027a78
painteyes <andreantoniocchipinti@gmail.com>
2021-11-16 16:10:38 +0100

    commit changes

--- Milestone-3/js/script.js
+++ Milestone-3/js/script.js
@@ -1,8 +1,8 @@
 // Consegna:
 // Scrivi un programma che stampi i numeri da 1 a 100.
 
-// MILESTONE 2
-// Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
+// MILESTONE 3
+// Applica uno stile differente a seconda del valore dell'indice per i multipli di 3, per i multipli di 5 e per i valori che sono sia multipli di 3 che di 5.
 
 for (let i = 1; i <= 100; i++) {
     
